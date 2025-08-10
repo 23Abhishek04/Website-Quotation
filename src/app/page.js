25b5e7cf16f8266a9b12b084e2cfa455bdaca213
23Abhishek04 <abhishekbamane23@gmail.com>
2025-08-10 19:03:01 +0530

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div className='w-full h-screen items-center justify-center bg-amber-100 flex'>
-
-      HElllo
-
-
+    <div className="w-full h-screen flex items-center justify-center flex-col bg-cyan-100">
+      <h1 className="text-3xl font-serif font-semibold">This is Sample Video</h1>
+      <video
+        src="videos/sample.mp4"
+        autoPlay
+        loop
+        muted
+        className=" h-[400px]  w-auto"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
